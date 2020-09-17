bdf53756bec3d796cece2b79871b4f15ace0d1b2
nisarg1499 <nisshah1499@gmail.com>
2020-09-17 21:35:56 +0530

    requirements.txt added and readme

--- project/crawl/tests.py
+++ project/crawl/tests.py
@@ -1,11 +1,14 @@
 from django.test import TestCase
 from .models import Blogs, History, User
+import datetime
+from django.utils import timezone
 
 class BlogsTestCase(TestCase):
 	def setUp(self):
 		# creating objects with few fields only
-		Blogs.objects.create(tag = "culture", title = "Title", content = "content1")
-		Blogs.objects.create(tag = "tech", title = "Technology", content = "New content")
+		d = datetime.datetime.now()
+		Blogs.objects.create(tag = "culture", title = "Title", pusblish_date = '2020-09-17 20:44:28.433459', content = "content1")
+		# Blogs.objects.create(tag = "tech", title = "Technology", pusblish_date = datetime(timezone.now), content = "New content")
 
 	def test_simple(self):
 		object1 = Blogs.objects.get(tag = "culture")
